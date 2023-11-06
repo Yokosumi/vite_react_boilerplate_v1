67824c3fda4e1b4698d7cef8acbac3a90a37cacb
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-06 10:12:17 +0100

    tailwind in

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import "./App.scss";
 
 function App() {
- return (
+  return (
     <>
-     <h1>Hello World</h1>
-  </>
-  )
+      <h1>Hello World</h1>
+    </>
+  );
 }
 
-export default App
+export default App;
