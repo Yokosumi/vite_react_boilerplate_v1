ab8c5d2659747dfee4e693fc54e607c697f47b8a
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-12-14 17:56:17 +0100

    changed style + added prettier config + added initial text

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from "react";
-
 function App() {
   return (
     <>
-      <h1 className="text-5xl text-red-500">Hello World</h1>
+      <div className="flex flex-col gap-10 justify-center items-center text-white my-4">
+        <h1 className="text-5xl">Template: React + TypeScript + Tailwind</h1>
+        <p className="w-[70ch] text-3xl">
+          This template is powered by Vite, a fast and opinionated frontend
+          build tool. Leveraging the efficiency of React for dynamic UIs, the
+          robustness of TypeScript for static typing, and the utility-first
+          approach of Tailwind CSS for styling, this combination crafted a
+          modern and streamlined development environment.
+        </p>
+      </div>
     </>
   );
 }
